8cb5a6b38a3825bed85bf57cfac492a842f189d6
GORLeex <phamxuanle833@gmail.com>
2026-03-17 22:28:16 +0700

    add 17/03

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,7 +1,64 @@
 package commons;
 
-public class BasePage {
-    public void clickToElement(){
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
+public class BasePage {
+    private WebDriver driver;
+    protected void clickToElement(){
+        driver.findElement(By.xpath("")).click();
+    }
+    public String getElementText(){
+        return driver.findElement(By.xpath("")).getText();
+
+    }
+    public void sendkeyElement(String valueSendkey){
+        driver.findElement(By.xpath("")).sendKeys(valueSendkey);
+
+    }
+    public boolean isElementDisplay(){
+        return driver.findElement(By.xpath("")).isDisplayed();
+    }
+
+    public String getCurentUrl(){
+       return driver.getCurrentUrl();
+    }
+    public String getPageSource(){
+        return driver.getPageSource();
+    }
+    public void back(){
+        driver.navigate().back();
+    }
+    public void forward(){
+        driver.navigate().forward();
+    }
+    public void refresh(){
+        driver.navigate().refresh();
+    }
+    public Alert waitAlertPresence(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        return wait.until(ExpectedConditions.alertIsPresent());
+    }
+    public void acceptAlert(){
+        driver.switchTo().alert().accept();
+    }
+    public void cancelAlert(){
+        driver.switchTo().alert().dismiss();
+    }
+    public String getTextAlert(){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        return alert.getText();
+    }
+
+    public void sendkeyAlert(String valueAlert){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert.sendKeys(valueAlert);
     }
 }
